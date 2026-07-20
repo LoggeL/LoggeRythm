2efea02d392d66f6350f9de9e6308731a258514d
LoggeL <hyper.xjo@gmail.com>
2026-07-20 16:52:07 -0400

    Fix standalone player instrumentation host

---
+++ mobile/modules/loggerythm-player/android/src/androidTest/java/top/logge/loggerythm/player/LoggeRythmPlayerTestActivity.kt
@@ -0,0 +1,6 @@
+package top.logge.loggerythm.player
+
+import android.app.Activity
+
+/** Launcher target used only by the standalone library instrumentation host. */
+class LoggeRythmPlayerTestActivity : Activity()
